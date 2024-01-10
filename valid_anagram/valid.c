8b4b63dca56ff3a7a67d1b9c40c662232de56018
clement cipriani <ciprianiclement@outlook.com>
2024-01-10 12:42:46 +0900

    Did Valid_Anagram in c

---
+++ valid_anagram/valid.c
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isAnagram(char* s, char* t) {
+    int size = strlen(s);
+
+    if (strlen(s) != strlen(t)) {
+        return false;
+    }
+    int freqArr[26] = {0};
+    for (int i = 0; i < size; i++) {
+        freqArr[(int)s[i] - 97]++;
+    }
+    for (int i = 0; i < size; i++) {
+        freqArr[(int)t[i] - 97]--;
+    }
+    for (int i = 0; i < 26; i++) {
+        if (freqArr[i] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
