2a7daa74a7f39e285f9670cf09f8e0cb89eafc9b
clement cipriani <ciprianiclement@outlook.com>
2024-01-19 12:25:36 +0900

    Did car_fleet.py

---
+++ car_fleet/car_fleet.py
@@ -0,0 +1,14 @@
+def carFleet(target, position, speed):
+        cars = [[p, s] for p, s in zip(position, speed)]
+        stack = []
+        print(sorted(cars)[::-1])
+        for p, s in sorted(cars)[::-1]:
+            stack.append((target - p) / s)
+            if len(stack) > 1 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+
+if __name__ == "__main__":
+    target = int(input())
+    position, speed = list(map(int, input().split())), list(map(int, input().split()))
+    print(carFleet(target, position, speed))
