5d50225396931029f1d958993590eea643852f48
clement cipriani <ciprianiclement@outlook.com>
2024-01-19 18:47:52 +0900

    Did largest_rectangle.py

--- car_fleet/car_fleet.py
+++ car_fleet/car_fleet.py
@@ -1,7 +1,6 @@
 def carFleet(target, position, speed):
         cars = [[p, s] for p, s in zip(position, speed)]
         stack = []
-        print(sorted(cars)[::-1])
         for p, s in sorted(cars)[::-1]:
             stack.append((target - p) / s)
             if len(stack) > 1 and stack[-1] <= stack[-2]:
---
+++ largest_rectangle/largest_rectangle.py
@@ -0,0 +1,17 @@
+def largestRectangleArea(heights):
+        stack = []
+        max_area = 0
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                max_area = max(max_area, height * (i - index))
+                start = index
+            stack.append((start, h))
+        for i, h in stack:
+            max_area = max(max_area, h * (len(heights) - i))
+        return max_area
+
+if __name__ == "__main__":
+    heights = list(map(int, input().split()))
+    print(largestRectangleArea(heights))
