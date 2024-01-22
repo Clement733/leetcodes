d4d945aa3255a10845cff90c750b13e2356f972b
clement cipriani <ciprianiclement@outlook.com>
2024-01-23 06:52:00 +0900

    Did search_sorted_array.py

---
+++ Binary_Search/search_sorted_array/search_sorted_array.py
@@ -0,0 +1,22 @@
+def search(nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[l] <= nums[m]:
+                if target > nums[m] or target < nums[l]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            else:
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return -1
+
+if __name__ == "__main__":
+    target = int(input())
+    nums = list(map(int, input().split()))
+    print(search(nums, target))
