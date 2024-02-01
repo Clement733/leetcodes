9e953a74fbacf2fde4fea85b64b7d426c562e9fd
clement cipriani <ciprianiclement@outlook.com>
2024-02-01 15:13:58 +0900

    Merged_two_lists

---
+++ Linked_Lists/merge_linked_lists/merge_linked_lists.py
@@ -0,0 +1,37 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def mergeTwoLists(self, list1, list2):
+    head = ListNode()
+    current = head
+    while list1 and list2:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+
+    current.next = list1 or list2
+    return head.next
+
+def mergeTwoLists(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        elif l2:
+            tail.next = l2
+        return dummy.next
