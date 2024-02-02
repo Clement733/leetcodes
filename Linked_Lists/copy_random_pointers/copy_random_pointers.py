d5738ae3900b3276477b57d135ef7906012f23b7
clement cipriani <ciprianiclement@outlook.com>
2024-02-02 15:12:28 +0900

    copyRandomList

---
+++ Linked_Lists/copy_random_pointers/copy_random_pointers.py
@@ -0,0 +1,15 @@
+def copyRandomList(head):
+        if not head:
+            return None
+        oldToCopy = {None:None}
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
+        cur = head
+        while cur:
+            oldToCopy[cur].next = oldToCopy[cur.next]
+            oldToCopy[cur].random = oldToCopy[cur.random]
+            cur = cur.next
+        return oldToCopy[head]
